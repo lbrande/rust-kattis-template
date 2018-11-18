a1203c3321cd551b10a496f680168a90284e84ff
lbrande <lovbra00@gmail.com>
2018-11-18 22:28:22 +0100

    Improved macro syntax.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #[allow(unused_macros)]
 macro_rules! scanln {
-    ($($var:ident),+) => ({
+    ($($var:ident),+) => {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
         let mut line = line.split_whitespace();
         $($var = line.next().unwrap().parse().unwrap();)+
-    });
-    ($max:expr,$($var:ident),+) => ({
+    };
+    ($max:expr,$($var:ident),+) => {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
         let mut line = line.splitn($max, char::is_whitespace);
         $($var = line.next().unwrap().parse().unwrap();)+
-    })
+    }
 }
 
 fn main() {}
